fee62ba3658b6750bbbf024e6a43294a67b92a1b
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2025-02-06 22:47:37 +0100

    add basic fight logic

---
+++ scripts/fight_logic.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready() -> void:
+	var enemy := PlayerVariables.enemy
+	var player := PlayerVariables.player
+
+	get_tree().root.add_child(enemy)
+	$PlayerContainer.add_child(player)
+
+func _process(_delta: float) -> void:
+	pass
