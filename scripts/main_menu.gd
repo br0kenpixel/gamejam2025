60198f457040d81e6de90eab7b1b313877a555f2
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2025-02-07 11:06:20 +0100

    add main menu button functionality

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -7,6 +7,9 @@ func _auto_size_bg():
 	var viewport_size := get_viewport_rect().size
 	$Background.size = viewport_size
 
+func _start_game() -> void:
+	get_tree().change_scene_to_file("res://scenes/new_world.tscn")
+
 func _process(_delta):
 	if Input.is_action_just_pressed("exit_game"):
 		_quit_game()
