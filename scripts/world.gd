04075e0c13210155d40fa531de9acc5ef3f8d4de
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2025-02-07 11:05:42 +0100

    finish title menu design

--- scripts/world.gd
+++ scripts/world.gd
@@ -3,11 +3,16 @@ extends Node2D
 func _ready() -> void:
 	if PlayerVariables.player != null:
 		$Player.insects = PlayerVariables.player.insects
+		$Player.level = PlayerVariables.player.level
+		print("syncing player instance")
 		PlayerVariables.player = null
 	
 	if PlayerVariables.enemy != null:
 		var children := get_children()
 		for child in children:
-			if child.name == PlayerVariables.enemy.name:
-				child.call_deferred("queue_free")
-				PlayerVariables.enemy = null
+			if child is Enemy:
+				if child.person_name == PlayerVariables.enemy.person_name:
+					child.call_deferred("queue_free")
+					print("syncing enemies")
+					PlayerVariables.enemy = null
+					return
